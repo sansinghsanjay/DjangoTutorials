fb449e645757242f948ecf8cf0a983888aa16af3
sansinghsanjay <san.singhsanjay@gmail.com>
2022-12-22 15:15:15 +0530

    removed unnecessary code and deleted useless files

--- CeleryRPCDemo/HomeApp/tasks.py
+++ CeleryRPCDemo/HomeApp/tasks.py
@@ -2,24 +2,6 @@ import time
 from celery import shared_task
 from CeleryRPCDemo.celery import app
 
-@shared_task
-def add(x, y):
-    return x + y
-
-@shared_task
-def send_email_task(email):
-    "background task to send an email asynchronously"
-    subject = 'Helo from Celery'
-    message = 'This is a test email sent asynchronously with Celery.'
-    time.sleep(5)
-    return send_mail(
-        subject,
-        message,
-        'alfabravo318@gmail.com',  # from
-        [email],  # to
-        fail_silently=False
-    )
-
 @app.task(bind=True)
 def loop(self, l):
     "simulate a long-running task like export of data or generateing a report"
