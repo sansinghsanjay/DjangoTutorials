af453c0eaed253660b2b8758cec40d2243b864cc
sansinghsanjay <san.singhsanjay@gmail.com>
2022-12-30 14:16:47 +0530

    formated the file

--- CeleryRPCDemo/HomeApp/tasks.py
+++ CeleryRPCDemo/HomeApp/tasks.py
@@ -4,11 +4,10 @@ from CeleryRPCDemo.celery import app
 
 @app.task(bind=True)
 def loop(self, l):
-    "simulate a long-running task like export of data or generateing a report"
+    #simulate a long-running task like export of data or generateing a report
     for i in range(int(l)):
         print(i)
         time.sleep(1)
-        self.update_state(state='PROGRESS',
-                          meta={'current': i, 'total': l})
+        self.update_state(state='PROGRESS', meta={'current': i, 'total': l})
     print('Task completed')
     return {'current': 100, 'total': 100, }
